6a3c5a92f7c9d28d2659141ede09f2f8799dba9e
ajopart-ajcreative <101755934+ajopart-ajcreative@users.noreply.github.com>
2023-06-07 13:41:03 +0200

    Y Bot animations

---
+++ Assets/KinetixSettings.cs
@@ -0,0 +1,17 @@
+using Kinetix;
+using UnityEngine;
+
+public class KinetixSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
