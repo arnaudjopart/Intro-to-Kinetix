cb3e476154f172d2bbe42c33fd876d91fc6aca48
ajopart-ajcreative <101755934+ajopart-ajcreative@users.noreply.github.com>
2023-06-07 14:19:04 +0200

    KinetixSettings

--- Assets/KinetixSettings.cs
+++ Assets/KinetixSettings.cs
@@ -1,17 +1,35 @@
+using System;
 using Kinetix;
 using UnityEngine;
 
 public class KinetixSettings : MonoBehaviour
 {
+    private const string API_Key = "206ceb29c16de50a447f4e1771bf2990";
+
+    private void Awake()
+    {
+        KinetixCore.OnInitialized += OnInit;
+    }
+
+    private void OnInit()
+    {
+        Debug.Log("OnInit");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        KinetixCore.Initialize(new KinetixCoreConfiguration()
+        {
+            PlayAutomaticallyAnimationOnAnimators = true,
+            EnableAnalytics = true,
+            EnableUGC = false,
+            VirtualWorldId = API_Key
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        KinetixCore.OnInitialized -= OnInit;
     }
 }
